be4fe7b4e30a0af9b21b9a089b1e41e82345316b
Damla Koksal <me@damlakoksal.com>
2022-04-17 10:35:57 +0300

    feat: add config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,14 @@ require("@nomiclabs/hardhat-waffle");
 // config here
 module.exports = {
   solidity: "0.8.4",
+  paths: {
+    // path defined for compiled smart contracts
+    artifacts: "./src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      // default chainId for MetaMask
+      chainId: 1337
+    }
+  }
 };
